df12b28b1911c027b3089cf563389685b0d7b432
Alessia Amitrano <alessia.amitrano@holition.com>
2019-10-18 00:07:40 +0100

    [wip] input to complete

--- src/app/components/input-text-page/input-text-page.component.ts
+++ src/app/components/input-text-page/input-text-page.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidationErrors
+} from '@angular/forms';
 
+const az09regex = /^[A-Za-z0-9 ]+$/;
 @Component({
   selector: 'app-input-text-page',
   templateUrl: './input-text-page.component.html',
   styleUrls: ['./input-text-page.component.scss']
 })
 export class InputTextPageComponent implements OnInit {
+  form: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      message: new FormControl('', [Validators.required, this.isValidText])
+    });
   }
 
+  private isValidText(control: FormControl): ValidationErrors {
+    const textExp: RegExp = az09regex;
+    if (!textExp.test(control.value)) {
+      return { invalidText: true };
+    }
+    return null;
+  }
 }
