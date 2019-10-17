dae103d0bf695b2c6f60b14a5e3202f1877c2a98
Alessia Amitrano <alessia.amitrano@holition.com>
2019-10-17 16:25:12 +0100

    [wip] basic containers and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { InputTextPageComponent } from './input-text-page/input-text-page.component';
+import { ResultPageComponent } from './result-page/result-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    InputTextPageComponent,
+    ResultPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/input-text-page/input-text-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-text-page',
+  templateUrl: './input-text-page.component.html',
+  styleUrls: ['./input-text-page.component.scss']
+})
+export class InputTextPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
