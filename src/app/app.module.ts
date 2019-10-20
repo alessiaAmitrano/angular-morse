e9f7ba165b9389d9021542741c49facaa976a1b4
aleami <alessia.amitranobo@gmail.com>
2019-10-20 14:33:52 +0100

    [wip]  form and button basic styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,16 @@ import { InputTextPageComponent } from './components/input-text-page/input-text-
 import { ResultPageComponent } from './components/result-page/result-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialUiModule } from './utils/material-ui/material-ui.module';
+import { InputFormComponent } from './ui/input-form/input-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     InputTextPageComponent,
-    ResultPageComponent
+    ResultPageComponent,
+    InputFormComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +28,9 @@ import { MaterialUiModule } from './utils/material-ui/material-ui.module';
     NgxsRouterPluginModule.forRoot(),
     NgxsReduxDevtoolsPluginModule.forRoot(),
     BrowserAnimationsModule,
-    MaterialUiModule
+    MaterialUiModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ui/input-form/input-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-form',
+  templateUrl: './input-form.component.html',
+  styleUrls: ['./input-form.component.scss']
+})
+export class InputFormComponent implements OnInit {
+  @Input() messageControl: FormControl;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
