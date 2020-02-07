c1f3a909fa54c44782274de6a81cd449bb7677a4
Alessia Amitrano <alessia.amitranobo@gmail.com>
2020-02-07 12:59:39 +0000

    remote api

--- src/app/core/services/morse-alphabet.service.ts
+++ src/app/core/services/morse-alphabet.service.ts
@@ -1,19 +1,21 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class MorseAlphabetService {
   // tslint:disable-next-line: variable-name
-  private _alphabetUrl = 'http://localhost:3000/data';
+  private _alphabetUrl = "https://api.myjson.com/bins/ikieo";
 
   // tslint:disable-next-line: variable-name
   constructor(private _http: HttpClient) {}
 
   getList(): Observable<any> {
-    return this._http.get<any>(this._alphabetUrl).pipe(map(data => data[0]));
+    return this._http
+      .get<any>(this._alphabetUrl)
+      .pipe(map(data => data.data[0]));
   }
 }
